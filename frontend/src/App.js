7826d3ffabea3dc2ec053ea6acb3b2f7f66c9728
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-07-18 12:48:56 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
   <>
-  <h1>hieee</h1>
+
+  <Header/>
+   <main>
+
+   <h1>hieee</h1>
+   </main>
+   
+   <Footer/>
+  
   
   </>
   );
