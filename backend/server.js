8578c85a207bbb4147c3e8be112e359cb56edc10
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-07-24 15:13:42 +0530

    es modules added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send('API is running')
+    res.send('API is running.....')
 })
 
 app.get('/api/products', (req, res) => {
@@ -21,5 +24,6 @@ res.json(product)
 
 })
 
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log('server running on'))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
