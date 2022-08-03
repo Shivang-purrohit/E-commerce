f7b11ecde13b2ef5cb3d65ff1f069d1d89703685
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-08-03 17:38:14 +0530

    done with Redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions';
 
-import axios from 'axios';
 
 const HomeScreen = () => {
- const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  const productList = useSelector(state => state.productList)
+
+  const { loading, error, products } = productList
+
+
 
  useEffect(() => {
+ dispatch(listProducts())
+
+ }, [dispatch] )
+
  
-  const fetchProducts = async () => {
-  const { data } = await axios.get('/api/products')
-
-  setProducts(data)
-
-  }
-
-  fetchProducts()
- 
-
-
- }, [] )
-
 
   return (
     <>
     <h1>Lastest Products</h1>
-    <Row>
+    { loading ? <Loader/> : error ?  <Message variant='danger'>{error}</Message> : 
+        <Row>
         {products.map(product =>(
             <Col key={product._id}  sm={12} md={6} lg={4} xl={3}  >
 
@@ -34,7 +35,10 @@ const HomeScreen = () => {
             </Col>
 
         ))}
-    </Row>
+    </Row> }
+      
+       
+    
       
     </>
   )
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,13 @@ import { createStore }  from 'redux'
 import  {  combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers' 
+const reducer = combineReducers({
 
-const reducer = combineReducers({})
+    productList: productListReducer,
+    productDetails: productDetailsReducer
+
+})
 
 const intialState = {}
 const middleware = [thunk]
