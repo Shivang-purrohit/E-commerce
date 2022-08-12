e4d5e9dd634f7d8d19b40509af99403c27dff42b
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-08-12 15:18:57 +0530

    error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,7 @@ const authUser = asyncHandler(async (req, res) => {
      
     const user = await User.findOne({ email })
 
-    if(user &&(await user.matchPassword(password)) ) {
+    if(user && (await user.matchPassword(password))) {
 
         res.json({
             _id: user._id,
@@ -22,7 +22,7 @@ const authUser = asyncHandler(async (req, res) => {
             isAdmin: user.isAdmin,
             token: generateToken(user._id)
         })
-    } else{
+    } else {
         res.status(401)
         throw new Error('Invalid email or password')
     }
@@ -35,21 +35,21 @@ const authUser = asyncHandler(async (req, res) => {
 
 const getUserProfile = asyncHandler(async (req, res) => {
    
-      const user = await User.findByID( req.user._id )
+      const user = await User.findByID(req.user._id)
 
-    if(user &&(await user.matchPassword(password)) ) {
+    if(user) {
 
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: generateToken(user._id)
+            
         })
-    } else{
-        res.status(401)
+    } else {
+        res.status(404)
         throw new Error('User not found')
     }
 })
 
-export { authUser, }
+export { authUser, getUserProfile }
