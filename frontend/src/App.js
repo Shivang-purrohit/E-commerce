a173f1556308aa47ca027e873d00d29be0d86fd2
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-08-17 01:07:42 +0530

    error in LoginScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import './bootstrap.min.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
 
@@ -19,12 +20,13 @@ const App = () => {
    <main className='py-3'>
 <Container>
 <Routes>
-<Route path= '/' element= {<HomeScreen/>}  />
 
-
+<Route path= '/login' element= {<LoginScreen/>} />
 <Route path= '/product/:id' element= {<ProductScreen/>} />
 <Route path= '/cart' element= {<CartScreen/>} />
 <Route path= '/cart/:id' element= {<CartScreen/>} />
+<Route path= '/' element= {<HomeScreen/>}  />
+
 
 
 </Routes>
