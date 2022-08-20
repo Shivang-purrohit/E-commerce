4fe64c2c67570ec371dc0b6b1a81b2dd77d9fff5
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-08-20 21:57:33 +0530

    Admin Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -126,4 +126,15 @@ const updateUserProfile = asyncHandler( async (req, res) => {
      
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+// @desc Get all users
+// @route   GET /api/users/
+// @access  Private/ADMIN
+
+const getUsers = asyncHandler( async (req, res) => {
+ 
+    const users = await User.find({})
+    res.json(users)
+
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers}
