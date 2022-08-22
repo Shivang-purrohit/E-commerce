5636d7d25b5b38b1dc84620e8587a5542a6a867e
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-08-22 15:10:41 +0530

    usersReducers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -137,4 +137,23 @@ const getUsers = asyncHandler( async (req, res) => {
 
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers}
+// @desc Delete user
+// @route   DELETE/api/users/:id
+// @access  Private/ADMIN
+
+const deleteUser = asyncHandler( async (req, res) => {
+ 
+    const user = await User.findById(req.params.id)
+
+      if(user) {
+        await user.remove()
+        res.json({ message: 'User removed'  })
+      } else {
+        res.status(404)
+        throw new Error('User not found')
+      }
+   
+
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
