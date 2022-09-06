8f26e9e90756054b2853f2ac059fa8d17592064a
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-09-06 21:13:30 +0530

    Revert "updated"
    This reverts commit 94663a60d8223620d7ac8a099c3dd8e88f60ef4b.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_REQUEST, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_REQUEST, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_CREATE_REVIEW_FAIL, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_REQUEST } from "../constants/productConstants"
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_REQUEST, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_REQUEST, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL } from "../constants/productConstants"
 import axios from 'axios'
 
 
@@ -142,41 +142,6 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     }
   }
 
-  
-  export const createProductReview = (id, review) => async (dispatch, getState) => {
-    try {
-      dispatch({
-        type: PRODUCT_CREATE_REVIEW_REQUEST
-      })
-  
-     const { userLogin: { userInfo }  } = getState()
-  
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-            
-            Authorization: `Bearer ${userInfo.token}`
-        }
-      }
-     await axios.post(`/api/products/${id}/reviews`, review , config )
-  
-      dispatch({
-        type: PRODUCT_CREATE_REVIEW_SUCCESS,
-       payload: data,
-      })
-  
-     
-    } catch (error) {
-        
-        dispatch({
-            type: PRODUCT_CREATE_REVIEW_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
-        })
-  
-    }
-  }
-
-  
   
   
   
